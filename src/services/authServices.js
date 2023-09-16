f15ddfbf63741743896c583e1db003d48e64b19f
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-09-16 13:15:58 +0530

    initLogin created

---
+++ src/services/authServices.js
@@ -0,0 +1,24 @@
+const initLogin = async (path) => {
+  try {
+    const response = await fetch(
+      "https://localhost:3000/api/v1/user/loginStatus",
+      {
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return {
+      err: err.response?.data.message || err.message,
+    };
+  }
+};
+
+export { initLogin };
