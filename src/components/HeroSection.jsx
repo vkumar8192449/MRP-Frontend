568dcb5cc76423f6a22e11d8249fbd5b22ea3ebc
subrajeet-maharana <subrajeet.info@gmail.com>
2023-12-20 01:34:34 +0530

    added minor adjustments

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const HeroSection = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center h-screen">
-      <div className="max-w-4xl -mt-32">
+      <div className="max-w-4xl -mt-64">
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
           <p className='text-sm font-semibold text-gray-700'>
             MRP is now available in submission mode only!
